6da8de2677ad3e5aec611e7f8fa237de5ed99db3
rosalbavargaspadilla <62787975+rosalbavargaspadilla@users.noreply.github.com>
2020-03-31 01:43:43 -0600

    Métodos de retornos de fecha organización R

---
+++ app.js
@@ -0,0 +1,15 @@
+function retornarFecha()
+{
+  var fecha
+  fecha=new Date();
+  var cadena=fecha.getDate()+'/'+(fecha.getMonth()+1)+'/'+fecha.getFullYear();
+  return cadena;
+}
+
+function retornarHora()
+{
+  var fecha
+  fecha=new Date();
+  var cadena=fecha.getHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds();
+  return cadena; 
+}
