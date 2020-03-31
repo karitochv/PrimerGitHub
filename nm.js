468594a937e6a73d14a9b91afe7c88eb9852280c
rosalbavargaspadilla <62787975+rosalbavargaspadilla@users.noreply.github.com>
2020-03-31 01:41:58 -0600

    Moficacion js en organizacion Perfil R

---
+++ nm.js
@@ -0,0 +1,7 @@
+function mostrarMensajeR(){
+alert('Bienvenido al curso JavaScript de aprenderaprogramar.com');
+}
+
+function mostrarMensajeR2(){
+alert('Ha hecho click sobre el párrafo inferior');
+}
