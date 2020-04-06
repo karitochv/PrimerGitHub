e9020097ef16f32e148aff089b4849920211ef29
karitochv <karitochv@hotmail.com>
2020-04-05 22:26:47 -0600

    Add Changes Karol

--- nm.js
+++ nm.js
@@ -5,3 +5,6 @@ alert('Bienvenido al curso JavaScript de aprenderaprogramar.com');
 function mostrarMensajeR2(){
 alert('Ha hecho click sobre el párrafo inferior');
 }
+function mostrarMensajeR3(){
+    alert('Ha hecho click sobre el párrafo inferior');
+    }
